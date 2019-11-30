c34c3c0f7e1d2501c98a4352fc2030eec52f2842
Keisuke Nakayama <baskk4.dt@gmail.com>
2019-11-30 20:23:20 +0900

    Add to create new topic

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -2,7 +2,19 @@ class TopicsController < ApplicationController
   def new
   end
 
-  def create
-    render plain: params[:topic].inspect
+  def show
+    @topic = Topic.find(params[:id])
   end
+
+  def create
+    @topic = Topic.new(topic_params)
+
+    @topic.save
+    redirect_to @topic
+  end
+
+  private
+    def topic_params
+      params.require(:topic).permit(:title,:description)
+    end
 end
