3fea09d9563a46975ded917dfaee478f7b2ac2d2
Keisuke Nakayama <baskk4.dt@gmail.com>
2019-12-01 14:03:59 +0900

    Add to reply

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,6 +14,6 @@ class PostsController < ApplicationController
 
   private
     def post_params
-      params.require(:post).permit(:contributor, :text)
+      params.require(:post).permit(:contributor, :text, :post_id)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_30_134210) do
+ActiveRecord::Schema.define(version: 2019_12_01_042212) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2019_11_30_134210) do
     t.bigint "topic_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "post_id"
+    t.index ["post_id"], name: "index_posts_on_post_id"
     t.index ["topic_id"], name: "index_posts_on_topic_id"
   end
 
@@ -31,5 +33,6 @@ ActiveRecord::Schema.define(version: 2019_11_30_134210) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "posts", "posts"
   add_foreign_key "posts", "topics"
 end
